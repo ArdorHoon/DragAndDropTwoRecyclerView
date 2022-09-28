d40aa3229d67c67647c92bb702f14b478ca852bf
ArdorHoon <didgnsah7320@gmail.com>
2022-09-28 13:50:37 +0900

    refactoring code for generic adapter

--- app/src/main/java/com/ardor/draganddrop/listener/ItemModifyListener.kt
+++
@@ -1,8 +0,0 @@
-package com.ardor.draganddrop.listener
-
-import com.ardor.draganddrop.model.SimpleModel
-
-interface ItemModifyListener {
-    fun setTopData(list: MutableList<SimpleModel?>)
-    fun setBottomData(list: MutableList<SimpleModel?>)
-}
